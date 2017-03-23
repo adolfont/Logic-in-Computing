5afffdd4db21823f7c1cb323a6ecbd360e3eeb15
Adolfo Neto <adolfo.usp@gmail.com>
2017-03-23 12:21:59 -0300

    Create semantica-logica-proposicional.clj

---
+++ Programming/Clojure/semantica-logica-proposicional.clj
@@ -0,0 +1,283 @@
+;; Operadores Lógicos
+(defn land [lvalue-1 lvalue-2]
+   (* lvalue-1 lvalue-2)
+  )
+(defn lor [lvalue-1 lvalue-2]
+   (max lvalue-1 lvalue-2)
+)
+(defn lnot [lvalue]
+  (- 1 lvalue))
+
+;; apenas para poder usar recursivamente
+(defn valora [formula valoracao]
+)
+
+;; função auxiliar para e, ou e implicação
+(defn valora-conectivos-binarios [formula valoracao]
+  (let [conectivo (second formula)
+        esquerda (first formula)
+        direita (nth  formula 2)]
+    (condp = conectivo
+        :and  (land (valora esquerda valoracao) (valora direita valoracao))
+        :or   (lor (valora esquerda valoracao) (valora direita valoracao))
+        :implies (lor  (lnot (valora esquerda valoracao)) (valora direita valoracao))
+      )
+  )
+)
+
+;; função de valoração
+(defn valora [formula valoracao]
+   (if (not (vector? formula))
+       (formula valoracao)
+       (if (= :not (first formula))
+           (lnot (valora (second formula) valoracao))
+           (valora-conectivos-binarios formula valoracao))
+   )
+)
+
+;; Testes
+;; Lembrar de sempre usar o ":"
+(println (valora :p '{:p 1}))
+(println (valora :p '{:p 0}))
+(println (valora [:p :or [:not :p]] '{:p 0}))
+(println (valora [:p :or [:not :p]] '{:p 1}))
+(println (valora [:p :and [:not :p]] '{:p 0}))
+(println (valora [:p :and [:not :p]] '{:p 1}))
+(println (valora [:p :and [:not :q]] '{:p 1, :q 0}))
+(println (valora [:p :or [:not :q]] '{:p 0, :q 1}))
+(println (valora [:r :implies [:p :or [:not :q]]] '{:p 0, :q 1, :r 1}))
+
+
+
+(defn inteiro-para-binario [valor qtde-digitos]
+  (loop [digito 0 valor-em-processamento valor result '[]]
+    (if (or (< digito qtde-digitos) (> valor-em-processamento 0))
+     (recur (inc digito)
+            (quot valor-em-processamento 2)
+            (into [(rem valor-em-processamento 2)] result ) )
+     result
+    ))
+)
+
+
+;Testes
+;(inteiro-para-binario 0 3)
+;(inteiro-para-binario 0 2)
+;(inteiro-para-binario 0 1)
+;(inteiro-para-binario 0 0)
+;(inteiro-para-binario 8 4)
+;(inteiro-para-binario 8 5)
+
+
+(defn cria-valoracao-binaria [quantidade-atomos]
+    (loop [i 0 tresult '[]]
+      (if (< i (int (Math/pow 2 quantidade-atomos)))
+        (recur (+ i 1) (conj tresult (inteiro-para-binario i quantidade-atomos)))
+        tresult
+      )
+    )
+)
+
+;(cria-valoracao-binaria 3)
+
+(defn my-zipmap [keys values]
+   (into {} (map vector  keys values))
+)
+
+(defn lista-todas-as-valoracoes [atomos]
+  (let [valoracoes (cria-valoracao-binaria (count atomos) )
+        total-valoracoes (count valoracoes)]
+    (loop [i 0 result '[]]
+      (if (< i total-valoracoes)
+          (recur (+ i 1) (into [(my-zipmap atomos (nth valoracoes i))] result ))
+           (reverse result)
+      )
+    )
+  )
+)
+
+;(lista-todas-as-valoracoes '[:p :q :r])
+
+(defn tabela-verdade [formula atomos]
+  (let [todas-as-valoracoes (lista-todas-as-valoracoes atomos)
+        total-valoracoes (count todas-as-valoracoes)]
+    (loop [i 0 result '[]]
+       (if (< i total-valoracoes)
+           (recur (+ i 1)
+                  (conj result [(nth todas-as-valoracoes i)
+                    (valora formula (nth todas-as-valoracoes i))])
+           )
+           result
+      )
+    )
+  )
+)
+
+
+(println (tabela-verdade '[:p :and :q] '[:p :q]))
+
+(println (tabela-verdade '[:p :implies [:p :and :q]] '[:p :q]))
+
+
+;;;; SUBFORMULAS
+
+(defn subformulas-aux [formula result]
+  (if (vector? formula)
+      (if (= :not (first formula))
+          (subformulas-aux (second formula)
+                (conj result formula))
+          (subformulas-aux (first formula)
+                       (subformulas-aux (nth formula 2)
+                      (conj result formula)))
+      )
+      (conj result formula)
+  )
+)
+
+(defn subformulas [formula]
+  (set (subformulas-aux formula '[])))
+
+; Testes
+
+; (subformulas :p)
+;#{:p}
+; (subformulas [:not :p])
+;#{[:not :p] :p}
+
+;(subformulas [[:not :p] :and :q])
+;#{[[:not :p] :and :q] :q [:not :p] :p}
+
+;(subformulas [[:not :p] :and :p])
+;#{[[:not :p] :and :p] [:not :p] :p}
+
+(defn roda-testes-subformulas[]
+   (println "Testes subformulas")
+   (println
+   (if
+      (and
+        (= (subformulas :p) #{:p})
+        (= (subformulas [:not :p]) #{[:not :p] :p})
+        (= (subformulas [[:not :p] :and :q]) #{[[:not :p] :and :q] :q [:not :p] :p})
+        (= (subformulas [[:not :p] :and :p]) #{[[:not :p] :and :p] [:not :p] :p})
+        )
+      "Passou em todos!"
+      "Falhou em ao menos um!"
+    ))
+)
+
+(roda-testes-subformulas)
+
+
+
+;;;;;;;;; TAMANHO
+
+(defn tamanho [formula]
+  (if (vector? formula) (count (flatten formula))
+      1))
+
+(defn roda-testes-tamanho[]
+   (println "Testes tamanho")
+   (println
+   (if
+      (and
+        (= (tamanho :p) 1)
+        (= (tamanho [:not :p]) 2)
+        (= (tamanho [[:not :p] :and :q]) 4)
+        (= (tamanho [[:not :p] :and [:not :q]]) 5)
+        )
+      "Passou em todos!"
+      "Falhou em ao menos um!"
+    ))
+)
+
+(roda-testes-tamanho)
+
+
+;;;;;;;;; CLASSIFICACAO
+
+(defn tabela-verdade-apenas-valores [formula atomos]
+  (map second (tabela-verdade formula atomos)))
+
+(defn filtro-atomos [elemento]
+  (not (or (= elemento :and) (= elemento :or) (= elemento :not) (= elemento :implies))))
+
+(defn atomos [formula]
+  (set
+  (if (vector? formula)
+    (filter filtro-atomos (flatten formula))
+    (vector formula))))
+
+
+
+(defn satisfazivel? [formula]
+  (= 1 (some #{1} (tabela-verdade-apenas-valores formula (atomos formula)) ))
+)
+
+(defn falsificavel? [formula]
+  (= 0 (some #{0} (tabela-verdade-apenas-valores formula (atomos formula)) ))
+)
+
+(defn valida? [formula]
+  (not (falsificavel? formula))
+)
+
+(defn insatisfazivel? [formula]
+  (not (satisfazivel? formula))
+)
+
+
+(defn roda-testes-classificacoes[]
+   (println "Testes classificacoes")
+   (println
+   (if
+      (and
+        (= (tabela-verdade-apenas-valores :p '[:p]) '(0 1))
+        (= (filtro-atomos :and) false)
+        (= (filtro-atomos :or) false)
+        (= (filtro-atomos :implies) false)
+        (= (filtro-atomos :not) false)
+        (= (filtro-atomos :p) true)
+        (= (atomos :p) #{:p})
+        (= (atomos [:p :and :q]) #{:q :p})
+        (= (atomos [[:p :implies [:p :or :r] ] :and [:not :q]]) #{:q :r :p})
+        (= (satisfazivel? :p) true)
+        (= (satisfazivel? [:p :implies :q]) true)
+        (= (satisfazivel? [:p :and [:not :p]]) false)
+        (= (falsificavel? :p) true)
+        (= (falsificavel? [:p :implies :q]) true)
+        (= (falsificavel? [:p :or [:not :p]]) false)
+        (= (valida? [:p :implies :q]) false)
+        (= (valida? [:p :and [:not :p]]) false)
+        (= (valida? [:p :or [:not :p]]) true)
+        (= (insatisfazivel? [:p :implies :q]) false)
+        (= (insatisfazivel? [:p :and [:not :p]]) true)
+        (= (insatisfazivel? [:p :or [:not :p]]) false)
+        )
+      "Passou em todos!"
+      "Falhou em ao menos um!"
+    ))
+)
+
+(roda-testes-classificacoes)
+
+
+;; CONSEQUENCIA LOGICA
+
+
+(defn consequencia-logica [a b]
+  true)
+
+(defn roda-testes-consequencia-logica[]
+   (println "Testes Consequencia Logica")
+   (println
+   (if
+      (and
+         (= (consequencia-logica :p :p) true )
+         (= (consequencia-logica :p :q) false )
+        )
+      "Passou em todos!"
+      "Falhou em ao menos um!"
+    ))
+)
+
+(roda-testes-consequencia-logica)
